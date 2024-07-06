63a10d59cdea5e506ed21fe777570b6c21654b22
neojason <s10243654@connect.np.edu.sg>
2024-07-06 20:01:11 +0800

    Branch update to merge, design updates

--- src/pages/NewPage.js
+++ src/pages/NewPage.js
@@ -7,15 +7,23 @@ import Footer from "../components/Footer";
 import { PieChart, Pie, Cell, Tooltip, Legend, BarChart, Bar, XAxis, YAxis, CartesianGrid } from 'recharts';
 
 function NewPage() {
-    const [showDetails, setShowDetails] = useState(false);
-    const [data, setData] = useState(null);
-    const [showAllReviews, setShowAllReviews] = useState(false);
-    const [showAllKeyTopics, setShowAllKeyTopics] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
-    const [filter, setFilter] = useState('All');
-    const [selectedPhrase, setSelectedPhrase] = useState(null);
+    const [showDetails, setShowDetails] = useState(false); // State for showing product details
+    const [data, setData] = useState(null); // State for data from WebSocket
+    const [showAllReviews, setShowAllReviews] = useState(false); // State for showing all reviews
+    const [showAllKeyTopics, setShowAllKeyTopics] = useState(false); // State for showing all key topics
+    const [isLoading, setIsLoading] = useState(false); // State for loading circle
+    const [selectedPhrase, setSelectedPhrase] = useState(null); // State for selected key topic for key topics section
+    const [filter, setFilter] = useState('All Sentiments'); // State for filtering reviews by sentiment
+    const [selectedRating, setSelectedRating] = useState(null); // State for selected AI rating
+    const [selectedEmotion, setSelectedEmotion] = useState(null); // State for selected emotion
+    const [showFilterOptions, setShowFilterOptions] = useState(false); // State for showing filter options
+    const [keyTopicsFilter, setKeyTopicsFilter] = useState('All Sentiments'); // State for filtering key topics by sentiment
+    const [showKeyTopicsFilterOptions, setShowKeyTopicsFilterOptions] = useState(false); // State for showing key topics filter options
+    const [expandedTopics, setExpandedTopics] = useState({}); // State for expanded key topics for more comments
 
-    useEffect(() => {
+    
+
+    useEffect(() => { // Establish WebSocket connection
         const ws = new WebSocket('ws://localhost:8080');
 
         ws.onopen = () => {
@@ -38,37 +46,62 @@ function NewPage() {
         };
     }, []);
 
-    useEffect(() => {
+    useEffect(() => { // Set selected phrase to first key topic phrase
         if (data && data.key_topics && Object.keys(data.key_topics).length > 0) {
             const firstKey = Object.keys(data.key_topics)[0];
             setSelectedPhrase(firstKey);
         }
     }, [data]);
 
-    const toggleDetails = () => {
+    const toggleFilterOptions = () => { // Toggle show collapsible filter options for reviews
+        setShowFilterOptions(!showFilterOptions);
+    };
+    
+    const toggleKeyTopicsFilterOptions = () => { // Toggle show collapsible key topics filter options
+        setShowKeyTopicsFilterOptions(!showKeyTopicsFilterOptions);
+    };
+    
+
+    const toggleDetails = () => { // Toggle show details of product
         setShowDetails(!showDetails);
     };
 
-    const toggleShowAllReviews = () => {
+    const toggleShowAllReviews = () => { // Toggle show all reviews of product
         setShowAllReviews(!showAllReviews);
     };
 
-    const toggleShowAllKeyTopics = () => {
+    const toggleShowAllKeyTopics = () => { // Toggle show all key topics of product
         setShowAllKeyTopics(!showAllKeyTopics);
     };
 
-    const handlePhraseClick = (phrase) => {
+    const handlePhraseClick = (phrase) => { // Handle click on key topic phrase
         setSelectedPhrase(phrase);
     };
 
-    const COLORS = ['#87c187', '#F08080', '#ffd966'];
+    const toggleShowMoreComments = (topic) => { // Toggle show more comments for key topic
+        setExpandedTopics(prev => ({
+            ...prev,
+            [topic]: !prev[topic]
+        }));
+    };
 
-    const filteredReviews = data?.reviews?.filter(review => {
-        if (filter === 'All') return true;
-        return review.sentiment === filter;
+    const COLORS = ['#87c187', '#F08080', '#ffd966']; // Colors for PieChart 
+
+
+    const filteredReviews = data?.reviews?.filter(review => { // Filter reviews based on sentiment, rating, and emotion
+        const sentimentMatch = filter === 'All Sentiments' || review.sentiment === filter;
+        const ratingMatch = selectedRating === null || review["AI-rating"] === selectedRating;
+        const emotionMatch = selectedEmotion === 'All Emotions' || selectedEmotion === null || review.emotions === selectedEmotion.toLowerCase();
+        return sentimentMatch && ratingMatch && emotionMatch;
     }) || [];
+    
+    
+    
+    
+    
+    
 
-    const renderLoading = () => {
+    const renderLoading = () => { // Render loading icon
         return (
             <div className="flex justify-center mt-20">
                 <div className="loading-icon-container justify-self-center" customStyles={{width: '100%'}}>
@@ -87,18 +120,18 @@ function NewPage() {
         );
     };
 
-    const renderReviewSections = () => {
+    const renderReviewSections = () => { // Render review section
         if (!data) return null;
 
-        const { summary, key_topics } = data;
+        const { summary, key_topics } = data; // Destructure data
 
-        const pieData = [
+        const pieData = [ // Data for PieChart
             { name: 'Positive', value: summary["Percentage of Positive Reviews"] },
             { name: 'Negative', value: summary["Percentage of Negative Reviews"] },
             { name: 'Neutral', value: summary["Percentage of Neutral Reviews"] },
         ];
 
-        const barData = [
+        const barData = [ // Data for BarChart
             { name: 'Joy', value: summary["Number of Joy Emotions"], color: '#FFD700' },
             { name: 'Anger', value: summary["Number of Anger Emotions"], color: '#FF4500' },
             { name: 'Neutral', value: summary["Number of Neutral Emotions"], color: '#808080' },
@@ -107,14 +140,16 @@ function NewPage() {
             { name: 'Surprise', value: summary["Number of Surprise Emotions"], color: '#FFA500' }
         ];
 
-        const topEmotion = barData.reduce((prev, current) => (prev.value > current.value) ? prev : current);
+        const topEmotion = barData.reduce((prev, current) => (prev.value > current.value) ? prev : current); // Culculation for emotion based on count
 
-        const sortedKeyTopics = Object.keys(key_topics)
+        const emotions = ['Joy', 'Anger', 'Neutral', 'Sadness', 'Disgust', 'Surprise']; // Emotions array for filtering
+
+        const sortedKeyTopics = Object.keys(key_topics) // Sort key topics based on unique comment count
     .sort((a, b) => key_topics[b].unique_comment_count - key_topics[a].unique_comment_count)
     .slice(0, 5);
 
 
-        return (
+        return ( 
             <div>
                 <div>
                     <div className="flex items-center justify-center mb-8">
@@ -285,12 +320,51 @@ function NewPage() {
 
                         <div className="bg-custom-beige p-6 mb-6 m-2 rounded-lg shadow-md">
                             <h3 className="text-2xl font-bold mb-4">Reviews</h3>
-                            <div className="flex items-center mb-4">
-                                <button onClick={() => setFilter('Positive')} className={`ml-2 p-2 rounded ${filter === 'Positive' ? 'bg-green-500 text-white' : 'bg-gray-200'}`}>Positive</button>
-                                <button onClick={() => setFilter('Negative')} className={`ml-2 p-2 rounded ${filter === 'Negative' ? 'bg-red-500 text-white' : 'bg-gray-200'}`}>Negative</button>
-                                <button onClick={() => setFilter('Neutral')} className={`ml-2 p-2 rounded ${filter === 'Neutral' ? 'bg-yellow-500 text-white' : 'bg-gray-200'}`}>Neutral</button>
-                                <button onClick={() => setFilter('All')} className={`ml-2 p-2 rounded ${filter === 'All' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}>All</button>
-                            </div>
+
+                            <button onClick={toggleFilterOptions} className="mb-4 p-2 bg-custom-gray border border-black rounded">
+                                {showFilterOptions ? 'Close Options' : 'Filter Options'}
+                            </button>
+
+                            {showFilterOptions && (
+                                <div className="bg-custom-gray p-4 border border-black mb-4 rounded">
+                                    <div className="flex items-center mb-4">
+                                        <button onClick={() => setFilter('Positive')} className={`ml-2 p-2 rounded ${filter === 'Positive' ? 'bg-green-500 text-white' : 'bg-gray-200'}`}>Positive</button>
+                                        <button onClick={() => setFilter('Negative')} className={`ml-2 p-2 rounded ${filter === 'Negative' ? 'bg-red-500 text-white' : 'bg-gray-200'}`}>Negative</button>
+                                        <button onClick={() => setFilter('Neutral')} className={`ml-2 p-2 rounded ${filter === 'Neutral' ? 'bg-yellow-500 text-white' : 'bg-gray-200'}`}>Neutral</button>
+                                        <button onClick={() => setFilter('All Sentiments')} className={`ml-2 p-2 rounded ${filter === 'All Sentiments' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}>All Sentiments</button>
+                                    </div>
+
+                                    <div className="flex items-center mb-4">
+                                        {['Anger', 'Disgust', 'Fear', 'Joy', 'Neutral', 'Sadness', 'Surprise'].map((emotion, index) => (
+                                            <button
+                                                key={index}
+                                                onClick={() => setSelectedEmotion(emotion.toLowerCase())}
+                                                className={`ml-2 p-2 rounded ${selectedEmotion === emotion.toLowerCase() ? 'bg-purple-500 text-white' : 'bg-gray-200'}`}
+                                            >
+                                                {emotion}
+                                            </button>
+                                        ))}
+                                        <button onClick={() => setSelectedEmotion('All Emotions')} className={`ml-2 p-2 rounded ${selectedEmotion === 'All Emotions' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}>All Emotions</button>
+                                    </div>
+
+                                    <div className="flex items-center mb-4">
+                                        {[1, 2, 3, 4, 5].map(star => (
+                                            <svg
+                                                key={star}
+                                                onClick={() => setSelectedRating(star)}
+                                                className={`w-8 h-8 ${selectedRating >= star ? 'text-yellow-500' : 'text-gray-200'} cursor-pointer`}
+                                                fill="currentColor"
+                                                viewBox="0 0 20 20"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                            >
+                                                <path d="M9.049.927C9.433.292 10.567.292 10.951.927l2.364 4.787 5.288.768c.663.097.93.912.448 1.379L15.949 11.5l.898 5.24c.113.657-.578 1.154-1.157.846L10 15.347l-4.69 2.46c-.579.308-1.27-.189-1.157-.846l.898-5.24-3.843-3.64c-.483-.467-.215-1.282.448-1.38l5.288-.767L9.049.927z" />
+                                            </svg>
+                                        ))}
+                                        <button onClick={() => setSelectedRating(null)} className={`ml-2 p-2 rounded ${selectedRating === null ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}>All AI Ratings</button>
+                                    </div>
+                                </div>
+                            )}
+
                             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                                 {filteredReviews.slice(0, showAllReviews ? filteredReviews.length : 4).map((review, index) => (
                                     <div key={index} className="bg-white p-4 rounded-lg shadow-md mb-4">
@@ -310,22 +384,55 @@ function NewPage() {
                                 />
                             )}
                         </div>
+
                         <div className="bg-custom-beige p-6 mb-6 m-2 rounded-lg shadow-md">
                             <h3 className="text-2xl font-bold mb-4">Key Topics</h3>
-                            {Object.keys(key_topics).slice(0, showAllKeyTopics ? Object.keys(key_topics).length : 4).map((topic, index) => (
-                                <div key={index} className="bg-white p-4 rounded-lg shadow-md mb-4">
-                                    <h4 className="text-xl font-semibold">{topic}</h4>
-                                    <p><strong>Unique Comment Count:</strong> {key_topics[topic].unique_comment_count}</p>
-                                    <ul className="list-disc list-inside">
-                                        {key_topics[topic].comments.map((comment, idx) => (
-                                            <li key={idx}>
-                                                <p>{comment.body}</p>
-                                                <p><strong>Sentiment:</strong> {comment.sentiment}</p>
-                                                <p><strong>AI Rating:</strong> {comment["AI-rating"]}</p>
-                                            </li>
-                                        ))}
-                                    </ul>
+
+                            <button onClick={toggleKeyTopicsFilterOptions} className="mb-4 p-2 bg-custom-gray border border-black rounded">
+                                {showKeyTopicsFilterOptions ? 'Close Options' : 'Filter Options'}
+                            </button>
+
+                            {showKeyTopicsFilterOptions && (
+                                <div className="bg-custom-gray p-4 border border-black mb-4 rounded">
+                                    <div className="flex items-center mb-4">
+                                        <button onClick={() => setKeyTopicsFilter('Positive')} className={`ml-2 p-2 rounded ${keyTopicsFilter === 'Positive' ? 'bg-green-500 text-white' : 'bg-gray-200'}`}>Positive</button>
+                                        <button onClick={() => setKeyTopicsFilter('Negative')} className={`ml-2 p-2 rounded ${keyTopicsFilter === 'Negative' ? 'bg-red-500 text-white' : 'bg-gray-200'}`}>Negative</button>
+                                        <button onClick={() => setKeyTopicsFilter('Neutral')} className={`ml-2 p-2 rounded ${keyTopicsFilter === 'Neutral' ? 'bg-yellow-500 text-white' : 'bg-gray-200'}`}>Neutral</button>
+                                        <button onClick={() => setKeyTopicsFilter('All Sentiments')} className={`ml-2 p-2 rounded ${keyTopicsFilter === 'All Sentiments' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}>All Sentiments</button>
+                                    </div>
                                 </div>
+                            )}
+
+                            {Object.keys(key_topics)
+                                .sort((a, b) => key_topics[b].unique_comment_count - key_topics[a].unique_comment_count)
+                                .filter(topic => {
+                                    if (keyTopicsFilter === 'All Sentiments') return true;
+
+                                    return key_topics[topic].comments.some(comment => comment.sentiment === keyTopicsFilter);
+                                })
+                                .slice(0, showAllKeyTopics ? Object.keys(key_topics).length : 4)
+                                .map((topic, index) => (
+                                    <div key={index} className="bg-white p-4 rounded-lg shadow-md mb-4">
+                                        <h4 className="text-xl font-semibold">{topic}</h4>
+                                        <p><strong>Unique Comment Count:</strong> {key_topics[topic].unique_comment_count}</p>
+                                        <ul className="list-disc list-inside">
+                                            {key_topics[topic].comments.slice(0, expandedTopics[topic] ? key_topics[topic].comments.length : 3).map((comment, idx) => (
+                                                <li key={idx}>
+                                                    <p>{comment.body}</p>
+                                                    <p><strong>Sentiment:</strong> {comment.sentiment}</p>
+                                                    <p><strong>AI Rating:</strong> {comment["AI-rating"]}</p>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                        {key_topics[topic].comments.length > 3 && (
+                                            <Button
+                                                onClick={() => toggleShowMoreComments(topic)}
+                                                text={expandedTopics[topic] ? 'Collapse' : 'Expand'}
+                                                className="mt-2 p-2 text-sm"
+                                            />
+                                        )}
+
+                                    </div>
                             ))}
                             {Object.keys(key_topics).length > 4 && (
                                 <Button
@@ -335,6 +442,8 @@ function NewPage() {
                                 />
                             )}
                         </div>
+
+
                     </div>
                 </div>
                 <Footer/>
