a853bd6c14a4a9b6e6f83b5077738ec60c409f4e
Yang Kai Miguel Low <s10243592@connect.np.edu.sg>
2024-08-07 04:54:29 +0800

    Finished the features page.

--- src/pages/Features.js
+++ src/pages/Features.js
@@ -1,8 +1,45 @@
 import React from 'react';
 import Header from '../components/Header'; 
 import Footer from '../components/Footer'; 
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 const Features = () => {
+    const data = [
+        {
+          name: `Time-Saving Analysis`,
+          img: `${process.env.PUBLIC_URL}/images/feature/time.png`,
+          review: `Instead of manually sifting through countless reviews, our tool consolidates and visualizes the most relevant information, presenting it in an easy-to-digest format.`
+        },
+        {
+          name: `Trend Identification`,
+          img: `${process.env.PUBLIC_URL}/images/feature/trend.png`,
+          review: `Our advanced scraping technology identifies common sentiments, helping you stay ahead of market shifts and choose products that align with the latest trends.`
+        },
+        {
+          name: `Comprehensive Product Insights`,
+          img: `${process.env.PUBLIC_URL}/images/feature/insight.png`,
+          review: `Our AI-powered analysis enables you to make well-informed purchasing decisions based on real customer feedback.`
+        },
+        {
+          name: `Sentiment Analysis`,
+          img: `${process.env.PUBLIC_URL}/images/feature/sentiment.png`,
+          review: `Quickly gauge whether the general consensus is positive, negative, or neutral, and make informed decisions based on this data.`
+        },
+        {
+          name: `Detailed Visualizations`,
+          img: `${process.env.PUBLIC_URL}/images/feature/visual.png`,
+          review: `Benefit from detailed visualizations that summarize review data. These visual tools make it easier to interpret large volumes of comments and quickly grasp key insights.`
+        },
+        
+      ];
+      const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1
+      };
     return (
         <div className="min-h-screen flex flex-col">
             <Header />
@@ -21,43 +58,43 @@ const Features = () => {
 <section className="bg-white p-8">
     <div className="max-w-4xl mx-auto text-center">
         <h2 className="text-3xl font-bold mb-8 text-gray-800">Advantages</h2>
-        <p className="text-gray-700 text-lg mb-8">Here are some of the key advantages of using our platform:</p>
+        <p className="text-gray-700 text-lg mb-8">Our platform offers numerous benefits to ecommerce shoppers! Check us out!</p>
         <div className="flex justify-around items-center">
             {/* Advantage 1 */}
             <div className="flex flex-col items-center">
                 <div className="w-32 h-32 mb-4 rounded-full overflow-hidden">
                     <img
-                        src="https://via.placeholder.com/150" // Replace with your image URL
+                    src={`${process.env.PUBLIC_URL}/images/speed.png`} // Replace with your image URL
                         alt="Advantage 1"
                         className="w-full h-full object-cover"
                     />
                 </div>
-                <p className="font-bold text-gray-800">Advantage One</p>
-                <p className="text-gray-600">Description of the first advantage.</p>
+                <p className="font-bold text-gray-800">Speed</p>
+                <p className="text-gray-600">Save time taken to analyze comments.</p>
             </div>
             {/* Advantage 2 */}
             <div className="flex flex-col items-center">
                 <div className="w-32 h-32 mb-4 rounded-full overflow-hidden">
                     <img
-                        src="https://via.placeholder.com/150" // Replace with your image URL
+                    src={`${process.env.PUBLIC_URL}/images/insight.png`} // Replace with your image URL
                         alt="Advantage 2"
                         className="w-full h-full object-cover"
                     />
                 </div>
-                <p className="font-bold text-gray-800">Advantage Two</p>
-                <p className="text-gray-600">Description of the second advantage.</p>
+                <p className="font-bold text-gray-800">Insightful</p>
+                <p className="text-gray-600">Gives user greater insights to make informed decisions.</p>
             </div>
             {/* Advantage 3 */}
             <div className="flex flex-col items-center">
                 <div className="w-32 h-32 mb-4 rounded-full overflow-hidden">
-                    <img
-                        src="https://via.placeholder.com/150" // Replace with your image URL
-                        alt="Advantage 3"
-                        className="w-full h-full object-cover"
-                    />
-                </div>
-                <p className="font-bold text-gray-800">Advantage Three</p>
-                <p className="text-gray-600">Description of the third advantage.</p>
+                <img
+                src={`${process.env.PUBLIC_URL}/images/friendly.png`} // Replace with your image URL
+                alt="Advantage 3"
+                className="w-full h-full object-cover"
+            />                
+            </div>
+                <p className="font-bold text-gray-800">User friendly</p>
+                <p className="text-gray-600">Easy to understand graphical data representations.</p>
             </div>
         </div>
     </div>
@@ -66,68 +103,35 @@ const Features = () => {
 
 {/* Video and Features Section */}
 {/* Special Features Section */}
-<section className="bg-white p-8">
-    <div className="max-w-4xl mx-auto">
+<section className="bg-white p-8 w-screen overflow-hidden">
+    <div className="max-w-full mx-auto">
         {/* Centered Header */}
         <h2 className="text-3xl font-bold mb-8 text-gray-800 text-center">Special Features</h2>
-        
-        {/* Carousel of Cards */}
-        <div className="flex overflow-x-scroll space-x-4 pb-4">
-            {/* Card 1 */}
-            <div className="bg-gray-100 p-6 rounded-lg shadow-md min-w-[300px]">
-                <div className="flex items-center mb-4">
-                    <div className="w-16 h-16 rounded-full overflow-hidden">
-                        <img
-                            src="https://via.placeholder.com/150" // Replace with your image URL
-                            alt="Card Image 1"
-                            className="w-full h-full object-cover"
-                        />
-                    </div>
-                    <div className="ml-4">
-                        <h3 className="text-xl font-semibold text-gray-800">Card Header One</h3>
-                        <p className="text-gray-600">Body text describing the content of the first card.</p>
-                    </div>
-                </div>
-            </div>
-            {/* Card 2 */}
-            <div className="bg-gray-100 p-6 rounded-lg shadow-md min-w-[300px]">
-                <div className="flex items-center mb-4">
-                    <div className="w-16 h-16 rounded-full overflow-hidden">
-                        <img
-                            src="https://via.placeholder.com/150" // Replace with your image URL
-                            alt="Card Image 2"
-                            className="w-full h-full object-cover"
-                        />
-                    </div>
-                    <div className="ml-4">
-                        <h3 className="text-xl font-semibold text-gray-800">Card Header Two</h3>
-                        <p className="text-gray-600">Body text describing the content of the second card.</p>
-                    </div>
-                </div>
-            </div>
-            {/* Card 3 */}
-            <div className="bg-gray-100 p-6 rounded-lg shadow-md min-w-[300px]">
-                <div className="flex items-center mb-4">
-                    <div className="w-16 h-16 rounded-full overflow-hidden">
-                        <img
-                            src="https://via.placeholder.com/150" // Replace with your image URL
-                            alt="Card Image 3"
-                            className="w-full h-full object-cover"
-                        />
-                    </div>
-                    <div className="ml-4">
-                        <h3 className="text-xl font-semibold text-gray-800">Card Header Three</h3>
-                        <p className="text-gray-600">Body text describing the content of the third card.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <div className='w-full'>
+      <div className="mt-20">
+      <Slider {...settings}>
+                                {data.map((d) => (
+                                    <div key={d.name} className="text-black p-4">
+                                        <div className='bg-white h-[350px] w-[300px] mx-2 rounded-xl shadow-lg'>
+                                            <div className='h-48 bg-indigo-500 flex justify-center items-center rounded-t-xl'>
+                                                <img src={d.img} alt="" className="h-32 w-32 rounded-full" />
+                                            </div>
+                                            <div className="flex flex-col items-center justify-center p-4">
+                                                <p className="text-lg font-semibold">{d.name}</p>
+                                                <p className="text-center text-gray-600 text-sm">{d.review}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </Slider>      </div>
+      
+    </div>
 
         {/* Features Grid */}
         <div className="mt-8 flex flex-col md:flex-row">
             <div className="md:w-3/5 md:pr-8">
                 <p className="text-gray-700 text-lg mb-6">
-                    Discover the unique features that make our product stand out. Each feature is designed to enhance your experience and provide unparalleled value.
+                    Discover the unique features that make our AI-powered solution stand out. Each feature is designed to enhance your experience and provide unparalleled value.
                 </p>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                     {/* Feature 1 */}
@@ -136,8 +140,8 @@ const Features = () => {
                             1
                         </div>
                         <div className="ml-4">
-                            <h3 className="text-xl font-semibold text-gray-800">Feature One</h3>
-                            <p className="text-gray-600">Description of the first feature that provides an incredible benefit to the users.</p>
+                            <h3 className="text-xl font-semibold text-gray-800">Cutting-Edge Tech</h3>
+                            <p className="text-gray-600">We utilize the latest in AI and data science to offer state-of-the-art comment analysis, ensuring users benefit from the most advanced and reliable insights available.</p>
                         </div>
                     </div>
                     {/* Feature 2 */}
@@ -146,8 +150,8 @@ const Features = () => {
                             2
                         </div>
                         <div className="ml-4">
-                            <h3 className="text-xl font-semibold text-gray-800">Feature Two</h3>
-                            <p className="text-gray-600">Description of the second feature that provides an incredible benefit to the users.</p>
+                            <h3 className="text-xl font-semibold text-gray-800">NLP</h3>
+                            <p className="text-gray-600">Using cutting-edge NLP, our tool understands and processes natural language reviews, delivering concise summaries and highlighting key points that matter most to users.</p>
                         </div>
                     </div>
                     {/* Feature 3 */}
@@ -156,8 +160,8 @@ const Features = () => {
                             3
                         </div>
                         <div className="ml-4">
-                            <h3 className="text-xl font-semibold text-gray-800">Feature Three</h3>
-                            <p className="text-gray-600">Description of the third feature that provides an incredible benefit to the users.</p>
+                            <h3 className="text-xl font-semibold text-gray-800">Deep Learning</h3>
+                            <p className="text-gray-600">Our platform leverages advanced deep learning techniques to accurately analyze and interpret customer comments, providing users with insightful sentiment analysis and product feedback.</p>
                         </div>
                     </div>
                     {/* Feature 4 */}
@@ -166,21 +170,21 @@ const Features = () => {
                             4
                         </div>
                         <div className="ml-4">
-                            <h3 className="text-xl font-semibold text-gray-800">Feature Four</h3>
-                            <p className="text-gray-600">Description of the fourth feature that provides an incredible benefit to the users.</p>
+                            <h3 className="text-xl font-semibold text-gray-800">Gen AI</h3>
+                            <p className="text-gray-600">By integrating generative AI, our tool not only analyzes but can also generate helpful summaries or alternative viewpoints based on user reviews, enhancing the decision-making process for users.</p>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="md:w-2/5">
-                <iframe
+            <iframe
                     className="w-full h-64 md:h-full rounded-lg"
-                    src="https://www.youtube.com/embed/example_video_id" // Replace with your video URL
+                    src="https://www.youtube.com/embed/5LkUtBlaW18" // Replace with your embeddable video URL
                     title="Video"
                     frameBorder="0"
                     allowFullScreen
-                ></iframe>
-            </div>
+                ></iframe>            
+                </div>
         </div>
     </div>
 </section>
@@ -203,15 +207,15 @@ const Features = () => {
             <div className="space-y-6">
                 <div>
                     <h3 className="text-xl font-semibold text-gray-800">Address</h3>
-                    <p className="text-gray-600">[Your Address Here]</p>
+                    <p className="text-gray-600">535 Clementi Road, Singapore 599489</p>
                 </div>
                 <div>
                     <h3 className="text-xl font-semibold text-gray-800">Phone</h3>
-                    <p className="text-gray-600">[Your Phone Number Here]</p>
+                    <p className="text-gray-600">8778 2611</p>
                 </div>
                 <div>
                     <h3 className="text-xl font-semibold text-gray-800">Email</h3>
-                    <p className="text-gray-600">[Your Email Here]</p>
+                    <p className="text-gray-600">commentwhiz2@gmail.com</p>
                 </div>
             </div>
         </div>
